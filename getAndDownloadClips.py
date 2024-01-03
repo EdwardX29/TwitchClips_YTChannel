7cca8ea8671ee877114666c616d3c0027cc93acb
Edward Xiao <edwardxiao29@gmail.com>
2024-01-03 15:17:38 -0500

    Update getAndDownloadClips.py

--- getAndDownloadClips.py
+++ getAndDownloadClips.py
@@ -1,33 +1,50 @@
+# Updated to newest Twitch API version
 from twitchAPI.twitch import Twitch
+from twitchAPI.helper import first
+import asyncio
 from TwitchCredentials import client_id, client_secret
 from datetime import datetime
-import requests
+import requests 
 
-# initialize Twitch client
-twitch = Twitch(client_id, client_secret)
+async def main():
+    twitch = await Twitch(client_id, client_secret)
+    user = await first(twitch.get_users(logins='CLIX'))
 
+    print(user.id)
 
-# date for the beginning date for searches
-start_date_string = "2022-07-01" # Your start date here
-start_date = datetime.strptime(start_date_string, "%Y-%m-%d", )
+    start_date_string = "2023-12-01" # Your start date here
 
-# date for the ending date for searches
-end_date_string="2022-07-31" # Your end date here
-end_date = datetime.strptime(end_date_string, "%Y-%m-%d", )
+    start_date = datetime.strptime(start_date_string, "%Y-%m-%d", )
 
-# get clip data
-clips = twitch.get_clips("173758090", first=22, started_at=start_date,
-                        ended_at=end_date)["data"]
-
-# download all clips into clips/ folder
-for id, clip in enumerate(clips):
+    end_date_string="2023-12-31" # Your end date here
+    end_date = datetime.strptime(end_date_string, "%Y-%m-%d", )
     
-    url = clip["thumbnail_url"].split("-preview-")[0] + ".mp4"
+    clips = twitch.get_clips(user.id, first=20, started_at=start_date, ended_at=end_date)
+    
+    i = 0
+    arr = []
+    async for clip in clips:
+        i += 1
+        print(clip.thumbnail_url)
+        print(clip.url)
+        print(clip.view_count)
+        arr.append(clip)
+        if i == 20:
+            break
+    
+    for id, clip in enumerate(arr):
+        url = clip.thumbnail_url.split("-preview-")[0] + ".mp4"
 
-    if "offset" in url:
-        url = "".join(url.split("AT-cm%7"))
+        if "offset" in url:
+            print("here")
+            url = "".join(url.split("AT-cm%7"))
 
-    mp4_url = url
-    content = requests.get(mp4_url).content 
-    with open(f"clips/{id}.mp4", "wb") as f:
-        f.write(content)
+        mp4_url = url
+        content = requests.get(mp4_url).content 
+        with open(f"clips/{id}.mp4", "wb") as f:
+            f.write(content)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
